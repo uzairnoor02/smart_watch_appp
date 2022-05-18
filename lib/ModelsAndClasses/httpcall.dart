4df61088aeb89310850146bde540c920d944e542
taimor12 <taimoorsaleem60@gmail.com>
2022-05-18 20:58:50 +0500

    Final Commit

--- lib/ModelsAndClasses/httpcall.dart
+++ lib/ModelsAndClasses/httpcall.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 Future<dynamic> myhttpcall(uri) async {
   var headers = {
     'Authorization':
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhCS1IiLCJzdWIiOiI5WVg2NVgiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNjUzMjQ2MTQ1LCJpYXQiOjE2NTI3MjIwNjZ9.v53gIZDAoBr3TSE_gOPzLVLo4TOOhQY2GCrU2NQEvcs",
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhCS1IiLCJzdWIiOiI5WVg2NVgiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNjUzMjQ2MTQ1LCJpYXQiOjE2NTI4ODk0MzF9.ojW3wWFCKOrA_Ol66MTyHfIduVQyO2lwWQ6FKQlc7K4",
     'Cookie':
         'fct=cfcc5b4ea1dd41cda946f0decd4d7f9c; JSESSIONID=8AB2FE903D69C660A11A5E53381942E6.fitbit1'
   };
